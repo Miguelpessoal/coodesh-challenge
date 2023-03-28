f22cbbaace57fbb8fc46e3fd9860801dc7f6bfff
Miguelpessoal <miguelpessoal12@gmail.com>
2023-03-28 10:40:29 -0300

    refactor: validate email duplicated

--- api/app/Controllers/Http/UsersController.ts
+++ api/app/Controllers/Http/UsersController.ts
@@ -6,6 +6,12 @@ export default class UsersController {
   public async store({ request, response }: HttpContextContract) {
     const payload = request.only(['name', 'email', 'password'])
 
+    const userExists = await User.findBy('email', payload.email)
+
+    if (userExists) {
+      return response.status(422).json('Email já cadastrado.')
+    }
+
     const user = await User.create(payload)
 
     return response.created(user)
